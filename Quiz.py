faa89a0a2abb75aed0534ce42ef4443ece9ab0f3
molikpunj <moulickpunjdu@gmail.com>
2025-07-31 22:29:51 +0530

    Added some questions and a print statement

--- Quiz.py
+++ Quiz.py
@@ -1,1 +1,16 @@
 '''WELCOME TO THE QUIZ GAME!!!'''
+
+questions = [
+    {
+        "quest" : "Q1. What is the full form of AI?",
+        "choice" : ["A. Articulate Information" "B. Artificial Intelligence" "C. Applied Informatics" "D. Arithmetic Indices"],
+        "correct" : "B"
+    },
+    {
+        "quest" : "Q2. What is the most famous AI tool as of 2025?",
+        "choice" : ["A. Claude", "B. Deepseek", "C. ChatGPT", "D. Gemini"],
+        "correct" : "C"
+    }
+]
+
+print(questions)
